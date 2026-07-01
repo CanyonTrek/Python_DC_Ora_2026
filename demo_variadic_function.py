2d79844f5b49baacd3ba3dbf527c871ecc3e9cc6
Donald Cameron <donald.cameron@qa.com>
2026-07-01 23:15:55 +0100

    Created demo_variadic_function.py

---
+++ demo_variadic_function.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+# Author: DCameron
+# Version: 1.0
+# Description: This script will demo HOWTO match strings/data using
+# str testing and pattern matching using the RE module
+"""
+    DocString
+"""
+import re
+
+# Example of a Variadic function that allows
+# variable number of parameters (TUPLE)
+def search_pattern(pattern=r"^.{19}$", *files):
+    lines = 0
+    reobj = re.compile(pattern)  # Compile PATTERN only ONCE
+    for file in files:
+        fh_in = open(file, mode="rt")
+
+        for line in fh_in:
+            # m = re.search(pattern, line)  # Match multiple patterns
+            m = reobj.search(line)  # Match Compiled pattern against line
+            if m:
+                lines =+ 1
+                print(f"Matched {m.group()}")
+    return lines
+
+search_pattern(r"^([A-Z]).*\1$", r"f:\labs\words", r"f:\labs\words2", r"f:\labs\words3")
